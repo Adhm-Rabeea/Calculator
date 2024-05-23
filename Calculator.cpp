4897347a4bd7a22b26762742344c62bbae9bcd70
Adhm Rabeea <adhmrabeea9@gmail.com>
2024-05-23 03:49:53 +0300

    first commit

---
+++ Calculator.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n1, n2;
+  char op = '1';
+  cout << "Calculator Is Open\n";
+  cout << "Enter [000] To Exit\n";
+  while (op != '0')
+  {
+    cin >> n1;
+    if (n1 == 000)
+      break;
+    cin >> op >> n2;
+    if (op == 'x' || op == '*' || op == '+' || op == '-' || op == '/')
+    {
+      switch (op)
+      {
+      case 'x':
+        cout << "= " << n1 * n2 << "\n";
+        break;
+      case '*':
+        cout << "= " << n1 * n2 << "\n";
+        break;
+      case '+':
+        cout << "= " << n1 + n2 << "\n";
+        break;
+      case '-':
+        cout << "= " << n1 - n2 << "\n";
+        break;
+      case '/':
+        cout << "= " << n1 / n2 << "\n";
+        break;
+      }
+    }
+    else
+      cout << "Error, Pleas Enter From This (x,*,+,-,/)\n";
+  }
+  return 0;
+}
